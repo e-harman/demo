dd251660c32af9cd5fc84d3f0215110854630060
eharman <harman.eric@gmail.com>
2015-08-20 04:58:39 +0000

    first check-in for pal_array.rb

---
+++ palindrome/pal_array.rb
@@ -0,0 +1,39 @@
+# pal_array.rb
+# Is an entered string a palindrome?
+# Created: 08/17/2015
+# Modified: 08/18/2015
+
+puts "Welcome to the palindrome test program. "
+print "Enter string: "
+
+original_string = gets.chomp
+
+cleaned_string = original_string.gsub(/\W/, '')
+cleaned_string.downcase!
+chars = cleaned_string.split ""
+
+# for i in 0..chars.length
+#     puts chars[i]
+# end
+
+# puts "Debug: (chars.length/2-1) is equal to: #{(chars.length/2)-1}"
+# for i in 0..(chars.length/2)-1
+#  if chars[i] == chars[(chars.length-i)-1]
+#      puts "It's a match on #{i}"
+#  else
+#      puts "No match on #{i}"
+#  end
+# end
+
+is_pal = true
+for i in 0..(chars.length/2)-1
+ if chars[i] != chars[(chars.length-i)-1]
+     is_pal = false
+ end
+end
+
+if is_pal ==true
+    puts "Your string is a palindrome."
+else
+    puts "Your string is NOT a palindrome."
+end
